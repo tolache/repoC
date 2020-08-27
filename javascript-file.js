365218379c534f7b8ff271953992d03a565e12a9
tolache <anatoly.cherenkov@gmail.com>
2020-08-27 15:39:02 +0300

    add JS

---
+++ javascript-file.js
@@ -0,0 +1,4 @@
+var img = document.createElement("img");
+img.src = "Bank Account Management Dashboard - Content Verification -- should display container title as [Manage Bank Accounts for ACH Payments] (failed).png";
+var src = document.getElementById("header");
+src.appendChild(img);
